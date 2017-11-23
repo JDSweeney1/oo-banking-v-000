38fe378f9cb93a8ec415ead1cabd79112cd60b1c
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-23 17:24:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if !sender.valid?
+    if sender.valid? == false
       "Transaction rejectd. Please chekc you account balance."
     elsif self.counter == 0
       sender.balance = sender.balance - self.amount
