0e037a6c2724b3005bc936085d43d4df30124390
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-23 17:37:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,9 @@ class Transfer
     if sender.valid? == false
       "Transaction rejectd. Please chekc you account balance."
       self.status = "rejected"
+      return
     end
-    if self.status == "pending" && self.status != "rejected"
+    if self.status == "pending"
       sender.balance = sender.balance - self.amount
       receiver.balance = receiver.balance + self.amount
       self.status = "complete"
